a8f210e359071b3222691a55cbe48874f7b81247
runminty <zengzhenhuang@gmail.com>
2019-04-05 10:00:40 +1300

    add attributes table

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,36 @@
+// Update with your config settings.
+
 module.exports = {
+
   development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
     },
-    useNullAsDefault: true
+    useNullAsDefault:true
   },
 
-  test: {
-    client: 'sqlite3',
+  staging: {
+    client: 'postgresql',
     connection: {
-      filename: ':memory:'
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
-    useNullAsDefault: true
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user: 'username',
+      user:     'username',
       password: 'password'
     },
     pool: {
@@ -30,4 +41,5 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
-}
+
+};
